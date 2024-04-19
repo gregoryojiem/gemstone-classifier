9b336f482b12969618e3b9d3aabd1f8744d94c40
Gregory Ojiem <gro3228@rit.edu>
2024-04-19 19:12:49 -0400

    Finished writing classifier test function

--- test_classifier.m
+++ test_classifier.m
@@ -8,8 +8,13 @@ if ~exist(filename, 'dir')
 end
 
 % Variables to keep track of accuracy
+number_of_test_samples = 300;
 correct_count = 0;
 total_count = 0;
+label_count = 1;
+true_labels = cell(number_of_test_samples, 1);
+predicted_labels = cell(number_of_test_samples, 1);
+class_labels = {};
 
 % Loop over each subfolder for each type of gem
 % Start at i=3 to avoid looping over current/parent directories
@@ -19,20 +24,46 @@ for i=3 : length(main_folder)
    subfolder_path = strcat(filename, "/", subfolder);
    gem_images = dir(subfolder_path);
     
+   % If the class label hasn't been seen before add it to class label list
+   if length(gem_images) > 2 && ~ismember(subfolder, class_labels)
+       class_labels{label_count} = subfolder;
+       label_count = label_count + 1;
+   end
+
    % Loop over the testing images
    for j=3 : length(gem_images)
        % Get the path of the image to classify
        gem_path = strcat(subfolder_path, "/", gem_images(j).name);
        
-       % Classify the image and update classification statistics
+       % Classify the image
        classification = classify(gem_path);
+
+       % If the classification class label hasn't been seen before, add to
+       % class label list
+       if ~ismember(classification, class_labels)
+           class_labels{label_count} = classification;
+           label_count = label_count + 1;
+       end
+
+       % Update classification statistics 
        if strcmp(classification, subfolder)
            correct_count = correct_count + 1;
        end
        total_count = total_count + 1;
+       true_labels{total_count} = subfolder;
+       predicted_labels{total_count} = classification;
    end
 end
 
+% Adjust preallocated size to actual size
+true_labels = true_labels(1:total_count-1);
+predicted_labels = predicted_labels(1:total_count-1);
+
+% Sort class labels in descending alphabetical order
+class_labels = sort(class_labels);
+
 % Display stats about accuracy
-disp(strcat('Accuracy: ', num2str(correct_count/total_count*100), "%"))
+disp(strcat('Accuracy: ', num2str(correct_count/total_count*100), "%"));
+confusion_matrix = confusionmat(true_labels, predicted_labels);
+confusionchart(confusion_matrix, class_labels(1:label_count-1));
 end
